38e56401831a5ce2a3ba16175748e34ff760f052
JrSEngineer <jr.s.engineer@gmail.com>
2024-11-13 10:45:40 -0300

    Creating migration that adds authentication entity table to database / Creating authentication controller.

--- ProcraftAPI/Controllers/AuthenticationController.cs
+++ ProcraftAPI/Controllers/AuthenticationController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProcraftAPI.Data.Context;
+using ProcraftAPI.Security.Authentication;
 
 namespace ProcraftAPI.Controllers
 {
@@ -6,6 +8,39 @@ namespace ProcraftAPI.Controllers
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
+
+        private readonly ProcraftDbContext _context;
+
+        public AuthenticationController(ProcraftDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] ProcraftAuthentication data)
+        {
+            await _context.Authentication.AddAsync(data);
+
+            await _context.SaveChangesAsync();
+
+            return Created($"{this.HttpContext.Request.Path}", data);
+        }
+
+        [HttpGet("/{email}")]
+        public async Task<IActionResult> Authenticate(string email)
+        {
+            var authenticationData = await _context.Authentication.FindAsync(email);
+
+            if (authenticationData == null)
+            {
+                return NotFound(new
+                {
+                    Message = "Invalid credentials. Please, verify your data and try again."
+                });
+            }
+
+            return Ok(authenticationData);
+        }
 
     }
 }
