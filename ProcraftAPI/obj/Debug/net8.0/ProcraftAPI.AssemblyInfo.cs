f4055611de59ae21a174a9bbde59d6d44dcfbc5e
JrSEngineer <jr.s.engineer@gmail.com>
2024-11-14 14:07:47 -0300

    Preapring for deploy.

--- ProcraftAPI/obj/Debug/net8.0/ProcraftAPI.AssemblyInfo.cs
+++ ProcraftAPI/obj/Debug/net8.0/ProcraftAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProcraftAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64357456cd13ac5f56c58e2a99ff2dd952d344d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93d6da07fa968d288a455868b5bc5c8515d2aff8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProcraftAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProcraftAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
