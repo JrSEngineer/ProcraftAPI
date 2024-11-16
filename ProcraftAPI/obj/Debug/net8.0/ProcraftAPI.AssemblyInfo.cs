17d2848b724c8bca638f978af5986b6a746d457d
JrSEngineer <jr.s.engineer@gmail.com>
2024-11-16 13:01:28 -0300

    Creating process related entities.

--- ProcraftAPI/obj/Debug/net8.0/ProcraftAPI.AssemblyInfo.cs
+++ ProcraftAPI/obj/Debug/net8.0/ProcraftAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProcraftAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c231a231f3ecf7ef21171872947f91c68e19911b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e836e4d947dbde8d4666cc34554b126c4f6ac179")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProcraftAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProcraftAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
