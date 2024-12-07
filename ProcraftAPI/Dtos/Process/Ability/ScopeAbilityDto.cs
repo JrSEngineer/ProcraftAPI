aa00ea706893b46bc655d43149b5137a3f8a7eeb
JrSEngineer <jr.s.engineer@gmail.com>
2024-12-06 22:10:02 -0300

    Correcting misspelled property in scope ability dto.

--- ProcraftAPI/Dtos/Process/Ability/ScopeAbilityDto.cs
+++ ProcraftAPI/Dtos/Process/Ability/ScopeAbilityDto.cs
@@ -5,5 +5,5 @@ public record ScopeAbilityDto
     public Guid Id { get; init; }
     public string Name { get; init; } = string.Empty;
     public string Description { get; init; } = string.Empty;
-    public Guid ScpoeId { get; init; }
+    public Guid ScopeId { get; init; }
 }
