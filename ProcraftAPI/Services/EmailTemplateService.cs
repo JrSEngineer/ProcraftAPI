593d85763adeb5ffbb4bbc78d0d16b14dc59c47a
JrSEngineer <jr.s.engineer@gmail.com>
2024-12-08 17:02:19 -0300

    Redefining html template.

--- ProcraftAPI/Services/EmailTemplateService.cs
+++ ProcraftAPI/Services/EmailTemplateService.cs
@@ -4,13 +4,14 @@
     {
         public string SetHtmlTemplateValues(string user, string recoveryCode)
         {
-            var templatesPath = $"Templates/index.html";
+            //var templatesPath = $"Templates/index.html";
 
-            string htmlFile = File.ReadAllText(templatesPath) ?? "";
+            //string htmlFile = File.ReadAllText(templatesPath) ?? "";
 
-            htmlFile = htmlFile.Replace("--User--", user).Replace("--Code--", recoveryCode);
+            //htmlFile = htmlFile.Replace("--User--", user).Replace("--Code--", recoveryCode);
 
-            return htmlFile;
+            return 
+                $"< img src = 'https://procraft-assets.s3.sa-east-1.amazonaws.com/procraft_logo_for_email.png' alt = 'procraft_logo' width = '180' > <h2 style = 'text-align: center; padding-bottom: 2rem;'> Olá, {user}!</ h2 > < br > < h3 style = 'text-align: center; padding-bottom: 2rem;' > Seu código de recuperação é:</ h3 > < br > < p style = 'text-align: center; font-size:2rem;' > {recoveryCode} </ p >";
         }
     }
 }
